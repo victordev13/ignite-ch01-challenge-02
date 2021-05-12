839846a5a42fae1d98f0c92f4208fd7300ba0706
victordev13 <vtrcarvalho.13@gmail.com>
2021-05-11 21:00:34 -0300

    Componentes criados

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import { GenreResponseProps } from '../App';
+import { Dispatch, SetStateAction } from 'react';
+
+export function SideBar({
+    genres,
+    selectedGenreId,
+    setSelectedGenreId,
+}: {
+    genres: GenreResponseProps[];
+    selectedGenreId: number;
+    setSelectedGenreId: Dispatch<SetStateAction<number>>;
+}) {
+    function handleClickButton(id: number) {
+        setSelectedGenreId(id);
+    }
+
+    return (
+        <nav className="sidebar">
+            <span>
+                Watch<p>Me</p>
+            </span>
+
+            <div className="buttons-container">
+                {genres.map((genre) => (
+                    <Button
+                        key={String(genre.id)}
+                        title={genre.title}
+                        iconName={genre.name}
+                        onClick={() => handleClickButton(genre.id)}
+                        selected={selectedGenreId === genre.id}
+                    />
+                ))}
+            </div>
+        </nav>
+    );
 }
